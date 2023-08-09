49622bee467ed68f1868c30db1ce67043e345544
thugperfect <meenamani9444@gmail.com>
2023-08-09 11:54:48 +0000

    navbar usestate to change class

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react'
 function App() {
+var i = 1;
+  const [click,setclick] = useState(false)
+  const handleclick = () =>{
+    setclick(!click);
+    console.log(click);
+   
+  }
   return (
     <div className="App">
-      <div className="navbar-wrapper"></div>
+      <div className="navbar-wrapper" onClick={handleclick}></div>
       <div className="body"></div>
     </div>
   );
