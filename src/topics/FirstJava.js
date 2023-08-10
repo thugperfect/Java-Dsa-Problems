0224e82c8b462746e4ada3f31f4a46df57d441e4
thugperfect <meenamani9444@gmail.com>
2023-08-10 06:13:02 +0000

    styling...

--- src/topics/FirstJava.js
+++ src/topics/FirstJava.js
@@ -3,11 +3,26 @@ const FirstJava = () =>{
     return(<div className='map'>
         <div className="topics-logo">FirstJava</div>
         <div className='topics-body'>
-            <div className='topics-body-topics'></div>
-            <div className='topics-body-topics'></div>
-            <div className='topics-body-topics'></div>
-            <div className='topics-body-topics'></div>
-            <div className='topics-body-topics'></div>
+            <div className='topics-body-topics'>
+                <div className='topic-point'>1</div>
+                <div className='topic-point'>2</div>
+            </div>
+            <div className='topics-body-topics'>
+            <div className='topic-point'>3</div>
+                <div className='topic-point'>4</div>
+            </div>
+            <div className='topics-body-topics'>
+            <div className='topic-point'>5</div>
+                <div className='topic-point'>6</div>
+            </div>
+            <div className='topics-body-topics'>
+            <div className='topic-point'>7</div>
+                <div className='topic-point'>8</div>
+            </div>
+            <div className='topics-body-topics'>
+            <div className='topic-point'>9</div>
+                <div className='topic-point'>10</div>
+            </div>
             
 
         </div>
