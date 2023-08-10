265ad0aa43fd159288d4f1081ba5d152c7d88f0d
thugperfect <meenamani9444@gmail.com>
2023-08-10 09:00:24 +0000

    questions added for conditionnals and loops

--- src/topics/ConditionalsAndLoops.js
+++ src/topics/ConditionalsAndLoops.js
@@ -7,48 +7,110 @@ const ConditionalsAndLoops = () =>{
         <div className='topics-body-topics'>
             <div className='topic-point-left'>1
             
-            <div className='topic-jist-left'></div>
+            <div className='topic-jist-left'>Fibonacci Series In Java Programs</div>
             </div>
             <div className='topic-point-right'>2
             
-            <div className='topic-jist-right'></div></div>
+            <div className='topic-jist-right'>Subtract the Product and Sum of Digits of an Integer</div></div>
         </div>
         <div className='topics-body-topics'>
         <div className='topic-point-left'>3
-        <div className='topic-jist-left'></div>
+        <div className='topic-jist-left'>Input a number and print all the factors of that number (use loops).
+</div>
         </div>
             <div className='topic-point-right'>4
-            <div className='topic-jist-right'></div>
+            <div className='topic-jist-right'>Take integer inputs till the user enters 0 and print the sum of all numbers (HINT: while loop)</div>
             </div>
         </div>
         <div className='topics-body-topics'>
         <div className='topic-point-left'>5
-        <div className='topic-jist-left'></div>
+        <div className='topic-jist-left'>Take integer inputs till the user enters 0 and print the largest number from all.</div>
         </div>
             <div className='topic-point-right'>6
             
-            <div className='topic-jist-right'></div>
+            <div className='topic-jist-right'>Factorial Program In Java</div>
             </div>
         </div>
         <div className='topics-body-topics'>
         <div className='topic-point-left'>7
-        <div className='topic-jist-left'></div>
+        <div className='topic-jist-left'>Calculate Electricity Bill</div>
         </div>
             <div className='topic-point-right'>8
-            <div className='topic-jist-right'></div>
+            <div className='topic-jist-right'>Calculate Average Of N Numbers</div>
             </div>
         </div>
         <div className='topics-body-topics'>
         <div className='topic-point-left'>9
-        <div className='topic-jist-left'></div>
+        <div className='topic-jist-left'>Calculate Discount Of Product</div>
         </div>
             <div className='topic-point-right'>10
-            <div className='topic-jist-right'></div>
+            <div className='topic-jist-right'>Calculate Distance Between Two Points</div>
             </div>
+            </div>
+            <div className='topics-body-topics'>
+            <div className='topic-point-left'>11
+            
+            <div className='topic-jist-left'>Calculate Commission Percentage</div>
+            </div>
+
+            <div className='topic-point-right'>12
+            
+            <div className='topic-jist-right'>Calculate CGPA Java Program</div></div>
+        </div>
+            </div>
+
+           
+        <div className='topics-body-topics'>
+        <div className='topic-point-left'>13
+        <div className='topic-jist-left'>Sum Of N Numbers
+</div>
+        </div>
+            <div className='topic-point-right'>14
+            <div className='topic-jist-right'>Armstrong Number In Java</div>
+            </div>
+        </div>
+        <div className='topics-body-topics'>
+        <div className='topic-point-left'>15
+        <div className='topic-jist-left'>Find Ncr & Npr</div>
+        </div>
+            <div className='topic-point-right'>16
+            
+            <div className='topic-jist-right'>Reverse A String In Java</div>
+            </div>
+        </div>
+        <div className='topics-body-topics'>
+        <div className='topic-point-left'>17
+        <div className='topic-jist-left'>Find if a number is palindrome or not</div>
+        </div>
+            <div className='topic-point-right'>18
+            <div className='topic-jist-right'>HCF & LCM Of Two Numbers Program</div>
+            </div>
+        </div>
+
+  
+
+            <div className='topics-body-topics'>
+            <div className='topic-point-left'>19
+            
+            <div className='topic-jist-left'>Check Leap Year Or Not</div>
+            </div>
+            <div className='topic-point-right'>20
+            
+            <div className='topic-jist-right'>Write a program to print the sum of negative numbers, sum of positive even numbers and the sum of positive odd numbers from a list of numbers (N) entered by the user. The list terminates when the user enters a zero.</div></div>
+        </div>
+        <div className='topics-body-topics'>
+        <div className='topic-point-left'>21
+        <div className='topic-jist-left'>Kunal is allowed to go out with his friends only on the even days of a given month. Write a program to count the number of days he can go out in the month of August.
+</div>
+        </div>
+            
+        </div>
+            
+
+            
         </div>
         
 
-    </div>
-    </div>)
+   )
 }
 export default ConditionalsAndLoops
--- src/topics/FirstJava.js
+++ src/topics/FirstJava.js
@@ -9,45 +9,43 @@ const FirstJava = () =>{
             <div className='topics-body-topics'>
                 <div className='topic-point-left'>1
                 
-                <div className='topic-jist-left'></div>
+                <div className='topic-jist-left'>Write a program to print whether a number is even or odd, also take input from the user.</div>
                 </div>
 
                 <div className='topic-point-right'>2
                 
-                <div className='topic-jist-right'></div></div>
+                <div className='topic-jist-right'>Take name as input and print a greeting message for that particular name.</div></div>
             </div>
             <div className='topics-body-topics'>
             <div className='topic-point-left'>3
-            <div className='topic-jist-left'></div>
+            <div className='topic-jist-left'>Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.</div>
             </div>
                 <div className='topic-point-right'>4
-                <div className='topic-jist-right'></div>
+                <div className='topic-jist-right'>Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)</div>
                 </div>
             </div>
             <div className='topics-body-topics'>
             <div className='topic-point-left'>5
-            <div className='topic-jist-left'></div>
+            <div className='topic-jist-left'>Take 2 numbers as input and print the largest number.</div>
             </div>
                 <div className='topic-point-right'>6
                 
-                <div className='topic-jist-right'></div>
+                <div className='topic-jist-right'>Input currency in rupees and output in USD.</div>
                 </div>
             </div>
             <div className='topics-body-topics'>
             <div className='topic-point-left'>7
-            <div className='topic-jist-left'></div>
+            <div className='topic-jist-left'>To calculate Fibonacci Series up to n numbers.</div>
             </div>
                 <div className='topic-point-right'>8
-                <div className='topic-jist-right'></div>
+                <div className='topic-jist-right'>To find out whether the given String is Palindrome or not.</div>
                 </div>
             </div>
             <div className='topics-body-topics'>
             <div className='topic-point-left'>9
-            <div className='topic-jist-left'></div>
+            <div className='topic-jist-left'>To find Armstrong Number between two given number.</div>
             </div>
-                <div className='topic-point-right'>10
-                <div className='topic-jist-right'></div>
-                </div>
+             
             </div>
             
 
