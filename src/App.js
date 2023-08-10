b1a93acf2a9d3cf962b3419037c6463686f1a0dc
thugperfect <meenamani9444@gmail.com>
2023-08-10 04:55:12 +0000

    body cleared

--- src/App.js
+++ src/App.js
@@ -12,12 +12,9 @@ function App() {
 
 <div className='App'>
 <Navbar/>
-<div className="body">dsnjivndeji;nvji;dnjvi;vndji;cnjid;ncjk;dnji;c
-ds jkcnjidncjidnji;cndji;ncjkdn
-dscjkcndjcnjd;sncj;sdmio;vmosi
-dsckjcndsklcnjdsnvjvidnivomdionds
-jdicniodnciodniocndiocniodnvv
-dsjkcnjdksncjidsnoivmndsios</div>
+<div className="body">
+  
+</div>
 </div>
 
 </div>
