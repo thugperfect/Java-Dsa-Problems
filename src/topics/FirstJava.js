5c85babde64cbd29713393ac9475c1e2f9a9ec9f
thugperfect <meenamani9444@gmail.com>
2023-08-10 06:46:10 +0000

    problem question on hover...

--- src/topics/FirstJava.js
+++ src/topics/FirstJava.js
@@ -4,24 +4,46 @@ const FirstJava = () =>{
         <div className="topics-logo">FirstJava</div>
         <div className='topics-body'>
             <div className='topics-body-topics'>
-                <div className='topic-point'>1</div>
-                <div className='topic-point'>2</div>
+                <div className='topic-point'>1
+                
+                <div className='topic-jist-left'></div>
+                </div>
+                <div className='topic-point'>2
+                
+                <div className='topic-jist-right'></div></div>
             </div>
             <div className='topics-body-topics'>
-            <div className='topic-point'>3</div>
-                <div className='topic-point'>4</div>
+            <div className='topic-point'>3
+            <div className='topic-jist-left'></div>
+            </div>
+                <div className='topic-point'>4
+                <div className='topic-jist-right'></div>
+                </div>
             </div>
             <div className='topics-body-topics'>
-            <div className='topic-point'>5</div>
-                <div className='topic-point'>6</div>
+            <div className='topic-point'>5
+            <div className='topic-jist-left'></div>
+            </div>
+                <div className='topic-point'>6
+                
+                <div className='topic-jist-right'></div>
+                </div>
             </div>
             <div className='topics-body-topics'>
-            <div className='topic-point'>7</div>
-                <div className='topic-point'>8</div>
+            <div className='topic-point'>7
+            <div className='topic-jist-left'></div>
+            </div>
+                <div className='topic-point'>8
+                <div className='topic-jist-right'></div>
+                </div>
             </div>
             <div className='topics-body-topics'>
-            <div className='topic-point'>9</div>
-                <div className='topic-point'>10</div>
+            <div className='topic-point'>9
+            <div className='topic-jist-left'></div>
+            </div>
+                <div className='topic-point'>10
+                <div className='topic-jist-right'></div>
+                </div>
             </div>
             
 
