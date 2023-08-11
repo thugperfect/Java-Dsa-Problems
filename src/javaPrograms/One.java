f9659d94cfdf96e2b292cfc52dc370c53e82f14a
thugperfect <meenamani9444@gmail.com>
2023-08-11 06:09:16 +0000

    java files fetching

---
+++ src/javaPrograms/One.java
@@ -0,0 +1,7 @@
+package javaPrograms;
+
+public class One {
+    public static void main(String[] args) {
+        System.out.println("hello mf");
+    }
+}
--- src/programs/firstJava/one.js
+++ src/programs/firstJava/one.js
@@ -1,12 +1,35 @@
 import '../../styles/programs.css'
 import '../../styles/topics.css'
+import { useState,useEffect } from 'react'
 const FirstJavaOne = ({qn}) =>{
+
+        const [file,setFile] = useState()
+    
+        useEffect(()=>{
+        async function fetchFile() {
+            try{
+        const res = await fetch('../../javaPrograms/One.java')
+        const cont = await res.text();
+        console.log(cont);
+        setFile(cont)
+        }
+        catch(err){
+        console.log(err);
+        }
+
+    }
+     
+    fetchFile() 
+  },[])
     return(<div className="program-body">
         <div className='topics-logo'>{qn}</div>
         <div className='program-content'>
 
-            <div className='program-hint'></div>
-            <div className='program-hint program'></div>
+          
+            <div className='program-hint program'>
+           {file}
+
+            </div>
         </div>
     
     </div>)
