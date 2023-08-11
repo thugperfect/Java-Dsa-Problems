a574b7cd30e1a30bd5ecfd2c78939e1042d9bdf3
thugperfect <meenamani9444@gmail.com>
2023-08-11 06:58:35 +0000

    removed unwanted imports...

--- src/App.js
+++ src/App.js
@@ -3,15 +3,7 @@ import './styles/App.css';
 import Navbar from './components/Navbar';
 import Topbar from './components/Topbar';
 import FirstJava from './topics/FirstJava';
-import FirstJavaOne from './programs/firstJava/one';
-import FirstJavaTwo from './programs/firstJava/two';
-import FirstJavaThree from './programs/firstJava/three';
-import FirstJavaFour from './programs/firstJava/four';
-import FirstJavaFive from './programs/firstJava/five';
-import FirstJavaSix from './programs/firstJava/six';
-import FirstJavaSeven from './programs/firstJava/seven';
-import FirstJavaEight from './programs/firstJava/eight';
-import FirstJavaNine from './programs/firstJava/nine';
+
 
 
 import { useState } from "react"
