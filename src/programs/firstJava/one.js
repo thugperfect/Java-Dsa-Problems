56359971a5da8b3a2b76be904c61d6fdfa6795e3
thugperfect <meenamani9444@gmail.com>
2023-08-11 07:34:35 +0000

    back to map logic

--- src/programs/firstJava/one.js
+++ src/programs/firstJava/one.js
@@ -1,8 +1,12 @@
 import '../../styles/programs.css'
 import '../../styles/topics.css'
-
+import FirstJava from '../../topics/FirstJava'
 import { useState,useEffect } from 'react'
-const FirstJavaOne = ({qn}) =>{
+const FirstJavaOne = ({qn,backPage}) =>{
+
+    const back = (page) =>{
+        backPage(page)
+    }
 const url = window.location.href
 console.log(url);
         const [file,setFile] = useState("")
@@ -34,6 +38,9 @@ console.log(url);
          
 
             </div>
+
+            <div onClick={() => back(<FirstJava/>)} className='program-btn'>Go To Map</div>
+            <div className='program-btn'>Next</div>
         </div>
     
     </div>)
