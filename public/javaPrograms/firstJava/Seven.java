8b03f9c5bc60103b48203f02346c70c7a4b5851a
thugperfect <meenamani9444@gmail.com>
2023-08-12 16:08:31 +0000

    Seven.java added...

--- public/javaPrograms/firstJava/Seven.java
+++ public/javaPrograms/firstJava/Seven.java
@@ -1,9 +1,22 @@
-//To calculate Fibonacci 
-//Series up to n numbers.
+
 
 package javaPrograms.firstJava;
-
+import java.util.*;
 public class Seven {
-    
+public static void main(String[] args) {
+    Scanner s =new Scanner(System.in);
+    System.out.println("Enter max :");
+    int n =s.nextInt();
+    int a = 0;
+    int b =1;
+    System.out.print(b+",");
+    for (int i =0;i<=n;i++){
+       int c = a+b;
+        a = b;
+        b = c;
+      
+        System.out.print(c+",");
+    }
+}
 }
 
