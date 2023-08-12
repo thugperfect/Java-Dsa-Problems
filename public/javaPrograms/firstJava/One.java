ce0aa194a46d8d060abab79e82fb957c82af6aaf
thugperfect <meenamani9444@gmail.com>
2023-08-12 06:03:53 +0000

    overlapping styles changed...

--- public/javaPrograms/firstJava/One.java
+++ public/javaPrograms/firstJava/One.java
@@ -1,20 +1,21 @@
 
 package javaPrograms.firstJava;
-import java.util.*;;
+import java.util.*;
 public class One {
-
 public static void main(String[] args) {
-    Scanner s =new Scanner(System.in);
-    while(true){
-        System.out.print("Enter a number :");
-        int n = s.nextInt();
-        if(n%2 == 0){
-            System.out.println("Even Number...");
-        } 
-        else{
-            System.out.println("Odd Number...");
-        }
+Scanner s =new Scanner(System.in);
+while(true){
+    System.out.print("Enter a number :");
+    
+    int n = s.nextInt();
+    if(n%2 == 0){
+
+        System.out.println("Even Number...");
+    } 
+    else{
+        System.out.println("Odd Number...");
     }
+}
 
 }
 
