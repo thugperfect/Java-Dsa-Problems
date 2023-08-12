e1f12d32a1d23cabbf4b75a3694f61ec17a9bd8e
thugperfect <meenamani9444@gmail.com>
2023-08-12 17:51:08 +0000

    added nine.java...

--- public/javaPrograms/firstJava/Nine.java
+++ public/javaPrograms/firstJava/Nine.java
@@ -1,8 +1,28 @@
-//To find Armstrong Number
-//between two given number.
 
 package javaPrograms.firstJava;
-
+import java.util.*;
 public class Nine {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("enter limit :");
+        int limit = s.nextInt();
+        int rem;
+        for(int i = 1;i < limit;i++){
+            int clone = i;
+           
+            int rem3 = 0;
+            while(clone>0){
+
+                rem = clone%10;
+               rem3 = rem3+rem*rem*rem;
+               clone = clone/10;
+            }
+           if(i == rem3){
+            System.out.println(i);
+           }
+        }
+
+    }
+    
     
 }
