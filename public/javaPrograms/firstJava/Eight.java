ba6121ee013acf0c033b5e8ed9b6cbc2eba8e901
thugperfect <meenamani9444@gmail.com>
2023-08-12 16:18:03 +0000

    added Eight.java

--- public/javaPrograms/firstJava/Eight.java
+++ public/javaPrograms/firstJava/Eight.java
@@ -3,7 +3,27 @@
 //Palindrome or not.
 
 package javaPrograms.firstJava;
-
+import java.util.*;
 public class Eight {
+public static void main(String[] args) {
+Scanner s = new Scanner(System.in);
+while(true){
+    System.out.print("Enter str :");
+    String str  = s.nextLine();
+    int len =str.length() -1;
+    String rev ="";
+    for (int i = len;i>=0;i--){
+        rev = rev + str.charAt(i);
+    }
+    if(str.equals(rev)){
+        System.out.println("palindrome");
+    }
+    else{
+        System.out.println("not a palindrome");
+    }
+}
+ 
     
+
+}
 }
