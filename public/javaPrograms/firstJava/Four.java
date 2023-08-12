c8751a0898a2e7ffffb1bc9eac56756521a182e5
thugperfect <meenamani9444@gmail.com>
2023-08-12 15:51:26 +0000

    four.java

--- public/javaPrograms/firstJava/Four.java
+++ public/javaPrograms/firstJava/Four.java
@@ -1,11 +1,32 @@
-
-//Take in two numbers and
-// an operator (+, -, *, /)
-// and calculate the value. 
-//(Use if conditions)
-
+ 
 package javaPrograms.firstJava;
-
+import java.util.*;
 public class Four {
-    
+public static void main(String[] args) {
+Scanner s = new Scanner(System.in);
+while(true){
+    System.out.print("Enter two no's :");
+    int n1 = s.nextInt();
+    int n2 = s.nextInt();
+    System.out.print("set operator :");
+    char op =s.next().charAt(0);
+    if(op == '+'){
+        System.out.println(n1+""+op+""+n2+"="+(float)(n1+n2));
+    }
+    else if(op == '-'){
+        System.out.println(n1+""+op+""+n2+"="+(float)(n1-n2));
+    }
+    else if(op == '*'){
+        System.out.println(n1+""+op+""+n2+"="+(float)(n1*n2));
+    }
+    else if(op == '/'){
+        System.out.println(n1+""+op+""+n2+"="+(float)(n1/n2));
+    }
+    else{
+        System.out.println("operator invalid");
+    }
+}
+     
+        
+}
 }
