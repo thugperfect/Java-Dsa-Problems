fa629442ff90dfb76abe6087dfb448bafb896833
thugperfect <meenamani9444@gmail.com>
2023-08-12 16:01:36 +0000

    SIx.java

--- public/javaPrograms/firstJava/Six.java
+++ public/javaPrograms/firstJava/Six.java
@@ -1,8 +1,15 @@
-//Input currency in rupees
-//and output in USD.
+
 
 package javaPrograms.firstJava;
-
+import java.util.*;
 public class Six {
-    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        while(true){
+            System.out.print("Enter INR :");
+            int inr = s.nextInt();
+            int usd = inr*85;
+            System.out.println("USD :"+usd);
+        }
+    }
 }
