026a6b4e707f9c08adf3e4a716f0f843c568c8a6
thugperfect <meenamani9444@gmail.com>
2023-08-12 05:55:34 +0000

    new body styles...

--- public/javaPrograms/firstJava/One.java
+++ public/javaPrograms/firstJava/One.java
@@ -1,10 +1,21 @@
 
-//Write a program to print
-//whether a number is
-//even or odd, also 
-//take input from the user.
 package javaPrograms.firstJava;
-
+import java.util.*;;
 public class One {
-  
+
+public static void main(String[] args) {
+    Scanner s =new Scanner(System.in);
+    while(true){
+        System.out.print("Enter a number :");
+        int n = s.nextInt();
+        if(n%2 == 0){
+            System.out.println("Even Number...");
+        } 
+        else{
+            System.out.println("Odd Number...");
+        }
+    }
+
+}
+
 }
