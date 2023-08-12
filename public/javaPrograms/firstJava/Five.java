8494bc69dead82b24a4bf39e0f6e8d48aa4544b5
thugperfect <meenamani9444@gmail.com>
2023-08-12 15:58:45 +0000

    Five.java

--- public/javaPrograms/firstJava/Five.java
+++ public/javaPrograms/firstJava/Five.java
@@ -1,30 +1,27 @@
-//Take 3 numbers as input
-// and print the largest number.
 
 package javaPrograms.firstJava;
 import java.util.*;
 public class Five {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        while(true){
-            System.out.print("Enter 3 no's");
-            int n1 = s.nextInt();
-            int n2 = s.nextInt();
-            int n3 = s.nextInt();
-            int max;
-            if(n1>n2){
-                if(n1>n3){
-                   max = n1; 
-                }
-                else{
-                    max = n3;
-                }
+public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    while(true){
+        System.out.print("Enter 3 no's :");
+        int n1 = s.nextInt();
+        int n2 = s.nextInt();
+        int n3 = s.nextInt();
+        int max;
+        if(n1>n2){
+            if(n1>n3){
+               max = n1; 
             }
-            else {
-                max = n2;
+            else{
+                max = n3;
             }
-            System.out.println("max :"+max);
-
         }
+        else {
+            max = n2;
+        }
+        System.out.println("max :"+max);
+    }
     }
 }
