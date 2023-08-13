56fd97768078657743e3ca71a879b186ac129221
thugperfect <meenamani9444@gmail.com>
2023-08-13 10:52:00 +0000

    added Eight.java

--- public/javaPrograms/conditionalsAndLoops/Eight.java
+++ public/javaPrograms/conditionalsAndLoops/Eight.java
@@ -1,6 +1,27 @@
 //Calculate Average Of N Numbers
 package javaPrograms.conditionalsAndLoops;
 
+import java.util.Scanner;
+
 public class Eight {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+     
+     
+    int add = 0,i;
+    System.out.print("enter limit:");
+    int n =s.nextInt();
+    for(i = 1;i<=n;i++){
+        System.out.print("Enter "+i+" no:"); 
+        int no = s.nextInt();
+        add = add+no;
+    }
+    System.out.println("average :"+(add/n));
     
+   
+            
+      
+    }
+   
+
 }
