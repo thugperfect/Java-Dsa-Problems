fa17a44aa3afc8ae54b181a9cb270d66fd788b0c
thugperfect <meenamani9444@gmail.com>
2023-08-13 09:46:58 +0000

    four.java added

--- public/javaPrograms/conditionalsAndLoops/Four.java
+++ public/javaPrograms/conditionalsAndLoops/Four.java
@@ -2,7 +2,21 @@
 //enters 0 and print the sum of all 
 //numbers (HINT: while loop)
 package javaPrograms.conditionalsAndLoops;
-
+import java.util.Scanner;
 public class Four {
-    
+public static void main(String[] args) {
+Scanner s  =new Scanner(System.in);
+int count = 0,add =0;
+boolean boo = true;
+while(boo){
+    System.out.print("Enter "+count+"th no:");
+    int n = s.nextInt();
+    if(n == 0){
+        boo=false;
+    }
+    add  =  add+n;
+    count++;
+}
+System.out.println(add);
+}
 }
