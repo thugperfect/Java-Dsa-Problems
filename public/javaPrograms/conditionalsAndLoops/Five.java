655a016924e564e39f090e16deb84e4b2f889a1d
thugperfect <meenamani9444@gmail.com>
2023-08-13 09:54:33 +0000

    added Five.java

--- public/javaPrograms/conditionalsAndLoops/Five.java
+++ public/javaPrograms/conditionalsAndLoops/Five.java
@@ -2,7 +2,24 @@
 //user enters 0 and print the 
 //largest number from all.
 package javaPrograms.conditionalsAndLoops;
-
+import java.util.Scanner;
 public class Five {
-    
+   public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    boolean boo = true;
+    int max = 0,count = 0;
+
+    while(boo){
+        System.out.print("Enter "+count+"th no:");
+        int n = s.nextInt();
+        if(max<n){
+            max = n;
+        }
+        if(n ==0){
+            boo = false;
+        }
+        count++;
+    }
+    System.out.println(max);
+   } 
 }
