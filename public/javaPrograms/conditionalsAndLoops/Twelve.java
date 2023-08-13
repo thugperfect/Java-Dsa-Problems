897762b166b523e31a5aa30c2f852f99856b986d
thugperfect <meenamani9444@gmail.com>
2023-08-13 11:44:58 +0000

    added Twelvw.java

--- public/javaPrograms/conditionalsAndLoops/Twelve.java
+++ public/javaPrograms/conditionalsAndLoops/Twelve.java
@@ -1,6 +1,22 @@
 //Calculate CGPA Java Program
 package javaPrograms.conditionalsAndLoops;
 
+import java.util.Scanner;
+
 public class Twelve {
-    
+    public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
+        int add = 0,i;
+        System.out.print("enter limit:");
+        int n =s.nextInt();
+        for(i = 1;i<=n;i++){
+            System.out.print("Enter "+i+" no:"); 
+            int no = s.nextInt();
+            add = add+no;
+        }
+        double d =add/n;
+
+        System.out.println("average :"+d);
+        
+    }
 }
