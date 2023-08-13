7af5cec73b79ea19baa5bbee78b1fc0529569444
thugperfect <meenamani9444@gmail.com>
2023-08-13 10:57:54 +0000

    added Nine.java

--- public/javaPrograms/conditionalsAndLoops/Nine.java
+++ public/javaPrograms/conditionalsAndLoops/Nine.java
@@ -1,6 +1,17 @@
 //Calculate Discount Of Product
 package javaPrograms.conditionalsAndLoops;
 
+import java.util.Scanner;
+
 public class Nine {
-    
+    public static void main(String[] args) {
+        Scanner s =  new Scanner(System.in);
+        System.out.print("price rs ?:");
+        int price = s.nextInt();
+        System.out.print("Discount % ?:");
+        int dis = s.nextInt();
+        int reduce = price*dis/100;
+        int rp= price-reduce;
+        System.out.println("reduce price :"+rp);
+    }
 }
