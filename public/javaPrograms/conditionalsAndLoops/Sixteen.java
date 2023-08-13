263c81a22bff76f313c2759d6e9de53e471e0e2a
thugperfect <meenamani9444@gmail.com>
2023-08-13 12:22:07 +0000

    added Sixteen.java

--- public/javaPrograms/conditionalsAndLoops/Sixteen.java
+++ public/javaPrograms/conditionalsAndLoops/Sixteen.java
@@ -1,6 +1,23 @@
 //Reverse A String In Java
 package javaPrograms.conditionalsAndLoops;
 
+import java.util.Scanner;
+
 public class Sixteen {
-    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter String :");
+        String str = s.nextLine();
+        String rev = reverse(str);
+        System.out.println(rev);
+
+
+    }
+    static String reverse(String str){
+        String rev ="";
+        for(int i =str.length()-1;i>=0;i--){
+            rev = rev + str.charAt(i);
+        }
+        return rev;
+    }
 }
