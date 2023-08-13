dae40656b23e025bb4ea59e5a080241215de96be
thugperfect <meenamani9444@gmail.com>
2023-08-13 11:40:04 +0000

    added Eleven.java

--- public/javaPrograms/conditionalsAndLoops/Eleven.java
+++ public/javaPrograms/conditionalsAndLoops/Eleven.java
@@ -1,7 +1,17 @@
 
-//Calculate Commission Percentage
 package javaPrograms.conditionalsAndLoops;
 
+import java.util.Scanner;
+
 public class Eleven {
-    
+    public static void main(String[] args) {
+        Scanner s =  new Scanner(System.in);
+        System.out.print("price rs ?:");
+        int price = s.nextInt();
+        System.out.print("commission % ?:");
+        int dis = s.nextInt();
+        int reduce = price*dis/100;
+      
+        System.out.println("commmission :"+reduce);
+    }
 }
