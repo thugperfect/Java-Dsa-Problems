951667d0098f02f6280d82ceaee4b263ccadb36b
thugperfect <meenamani9444@gmail.com>
2023-08-13 10:31:08 +0000

    added Six.java

--- public/javaPrograms/conditionalsAndLoops/Six.java
+++ public/javaPrograms/conditionalsAndLoops/Six.java
@@ -1,6 +1,21 @@
 //Factorial Program In Java
 package javaPrograms.conditionalsAndLoops;
 
+import java.util.Scanner;
+
 public class Six {
-    
+public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    while(true){
+        System.out.print("Enter a no. :");
+        int n = s.nextInt();
+        long mul = 1l;
+        while(n>0){
+            mul = mul*n;
+            n-=1;
+        }
+        System.out.println(mul);
+    }
+
+}    
 }
