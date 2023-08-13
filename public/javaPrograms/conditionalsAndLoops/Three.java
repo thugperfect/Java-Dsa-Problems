acfc1d14bd8a928197d235f58bac0854f3b9e3f4
thugperfect <meenamani9444@gmail.com>
2023-08-13 09:37:44 +0000

    added Three.java

--- public/javaPrograms/conditionalsAndLoops/Three.java
+++ public/javaPrograms/conditionalsAndLoops/Three.java
@@ -1,7 +1,22 @@
 //Input a number and print all the 
 //factors of that number (use loops).
 package javaPrograms.conditionalsAndLoops;
-
+import java.util.Scanner;
 public class Three {
-    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        while(true){
+            System.out.print("Enter no. :");
+            int n = s.nextInt();
+            
+            for(int i = n;i>0;i--){
+                if(n%i == 0){
+                    System.out.print(i+",");
+                }
+            }
+            System.out.println("\n");
+        }
+        
+
+    }
 }
