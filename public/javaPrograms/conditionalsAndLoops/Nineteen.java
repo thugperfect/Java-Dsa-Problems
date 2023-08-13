3d0548cae910997b57a0fd842745bfe76ed51156
thugperfect <meenamani9444@gmail.com>
2023-08-13 13:17:58 +0000

    added NIneteen.java

--- public/javaPrograms/conditionalsAndLoops/Nineteen.java
+++ public/javaPrograms/conditionalsAndLoops/Nineteen.java
@@ -1,6 +1,19 @@
 //Check Leap Year Or Not
 package javaPrograms.conditionalsAndLoops;
 
+import java.util.Scanner;
+
 public class Nineteen {
-    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Year ? :");
+        int y = s.nextInt();
+        if(y%4 == 0){
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
+        
+    }
 }
