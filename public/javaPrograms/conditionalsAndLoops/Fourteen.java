0de868d3e71d7a5ce6932db27a493e2c3b6bbd0e
thugperfect <meenamani9444@gmail.com>
2023-08-13 11:55:23 +0000

    fourteen.java

--- public/javaPrograms/conditionalsAndLoops/Fourteen.java
+++ public/javaPrograms/conditionalsAndLoops/Fourteen.java
@@ -1,6 +1,26 @@
 //Armstrong Number In Java
 package javaPrograms.conditionalsAndLoops;
 
+import java.util.Scanner;
+
 public class Fourteen {
-    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter no:");
+        int n = s.nextInt();
+        int clone = n;
+        int remadd = 0;
+        while(clone>0){
+            int rem = clone%10;
+            int rem3 = rem*rem*rem;
+            remadd = remadd+rem3;
+            clone/=10;
+        }
+        if(n==remadd){
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
+      }
 }
