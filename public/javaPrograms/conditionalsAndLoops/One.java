c32baedc73c8da7c8c4f93a44bcce408f881c9fe
thugperfect <meenamani9444@gmail.com>
2023-08-13 09:31:08 +0000

    added Two.java

--- public/javaPrograms/conditionalsAndLoops/One.java
+++ public/javaPrograms/conditionalsAndLoops/One.java
@@ -1,6 +1,20 @@
-//Fibonacci Series In Java Programs
-package javaPrograms.conditionalsAndLoops;
 
+package javaPrograms.conditionalsAndLoops;
+import java.util.*;
 public class One {
-    
+    public static void main(String[] args) {
+      Scanner s = new Scanner(System.in);
+      System.out.print("Enter limit :");
+      int limit = s.nextInt();
+      int a =0;
+      int b = 1;
+      System.out.print(b+",");
+      for (int i = 0;i<limit;i++){
+        int c = a+b;
+        a = b;
+        b =c;
+        System.out.print(c+",");
+      }
+
+    }
 }
--- public/javaPrograms/conditionalsAndLoops/Two.java
+++ public/javaPrograms/conditionalsAndLoops/Two.java
@@ -1,7 +1,22 @@
-//Subtract the Product and Sum of Digits 
-//of an Integer
-package javaPrograms.conditionalsAndLoops;
 
+package javaPrograms.conditionalsAndLoops;
+import java.util.Scanner;
 public class Two {
-    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a no. :");
+        int n = s.nextInt();
+        int mul = 1;
+        int add  = 0;
+        while(n>0){
+            int rem = n%10;
+             mul = mul*rem;
+             add = add+rem;
+            n/=10;
+        }
+        System.out.println(
+            mul-add
+        );
+        
+    }
 }
