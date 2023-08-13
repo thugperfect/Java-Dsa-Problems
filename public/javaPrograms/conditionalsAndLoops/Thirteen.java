4158e698edfeb4b4f8831b84ef0601e7148a3fc5
thugperfect <meenamani9444@gmail.com>
2023-08-13 11:49:41 +0000

    added Thirteen.java

--- public/javaPrograms/conditionalsAndLoops/Thirteen.java
+++ public/javaPrograms/conditionalsAndLoops/Thirteen.java
@@ -1,6 +1,20 @@
 //Sum Of N Numbers
 package javaPrograms.conditionalsAndLoops;
 
+import java.util.Scanner;
+
 public class Thirteen {
-    
+    public static void main(String[] args) {
+       Scanner s =new Scanner(System.in);
+       System.out.print("Enter limit:");
+       int n = s.nextInt();
+       int i;
+       int add = 0;
+        for(i = 1;i<=n;i++){
+            System.out.print("no. "+i+":");
+            int no = s.nextInt();
+            add = add+no;
+        }
+        System.out.println(add);
+    }
 }
