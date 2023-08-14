5e931921a4650920e5873ab6f38acbd7f5fa15cf
thugperfect <meenamani9444@gmail.com>
2023-08-14 06:10:21 +0000

    added qns for funcitons

--- src/topics/Functions.js
+++ src/topics/Functions.js
@@ -1,5 +1,19 @@
 import '../styles/topics.css'
 const Functions = () =>{
+    const q1 = "1.Define two methods to print the maximum and the minimum number respectively among three numbers entered by the user."
+    const q2 = "2.Define a program to find out whether a given number is even or odd."
+    const q3 = "3.A person is eligible to vote if his/her age is greater than or equal to 18. Define a method to find out if he/she is eligible to vote."
+    const q4 = "4.Write a program to print the sum of two numbers entered by user by defining your own method."
+    const q5 = "5.Define a method that returns the product of two numbers entered by user."
+    const q6 = "6.Write a program to print the circumference and area of a circle of radius entered by user by defining your own method."
+    const q7 = "7.Define a method to find out if a number is prime or not."
+    const q8 = "8.Write a program that will ask the user to enter his/her marks (out of 100). Define a method that will display grades according to the marks entered"
+    const q9  = "9.Write a program to print the factorial of a number by defining a method named 'Factorial'."
+    const q10 = "10.Write a function to find if a number is a palindrome or not. Take number as parameter."
+    const q11 = "11.Write a function to check if a given triplet is a Pythagorean triplet or not."
+    const q12 = "12.Write a function that returns all prime numbers between two given numbers."
+    const q13 = "13.Write a function that returns the sum of first n natural numbers."
+    
     return(<div className='map'>
     <div className="topics-logo">Functions</div>
     <div className='topics-body'>
