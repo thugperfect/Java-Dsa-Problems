07386225c9d0a7c0bf316688743ae49744034e8d
thugperfect <meenamani9444@gmail.com>
2023-08-17 14:29:29 +0000

    added Eight.java

--- public/javaPrograms/functions/Eight.java
+++ public/javaPrograms/functions/Eight.java
@@ -1,4 +1,45 @@
 package javaPrograms.functions;
-class Eight {
 
+import java.util.Scanner;
+
+class Eight {
+    public static void main(String[] args) {
+        
+Scanner s = new Scanner(System.in);
+int n = s.nextInt();
+String grade = grade(n);
+System.out.println(grade);
+        
+    }
+static String grade(int marks){
+    String one = "O";
+    String two = "A+";
+    String three = "A";
+    String four = "B+";
+    String five = "B";
+    String six = "U";
+    String invalid  = "invalid input";
+    if(marks>=90){
+        return one;
+    }
+    else if(marks>80&& marks<90){
+        return two;
+    }
+   else if(marks>70&& marks<80){
+        return three;
+    }
+    else if(marks>60&& marks<70){
+        return four;
+    }
+    else if(marks>50&& marks<60){
+        return five;
+    }
+   else if(marks<50){
+    return six;
+   }
+   else{
+return invalid;
+   }
+
+}
 }
