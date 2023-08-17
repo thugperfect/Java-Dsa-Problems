dd46c18236c82b77ee88352149c5219a7114ed71
thugperfect <meenamani9444@gmail.com>
2023-08-17 13:27:42 +0000

    added Four.java

--- public/javaPrograms/functions/Four.java
+++ public/javaPrograms/functions/Four.java
@@ -1,4 +1,16 @@
 package javaPrograms.functions;
-class Four{
 
+import java.util.Scanner;
+
+class Four{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b= s.nextInt();
+        int sum = sum(a,b);
+        System.out.println("SUM="+sum);
+    }
+static int sum(int a ,int b){
+    return a+b;
+}
 }
