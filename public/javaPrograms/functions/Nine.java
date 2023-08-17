bef0c3df5da6aa418a83e83dbbd737f224cffb6e
thugperfect <meenamani9444@gmail.com>
2023-08-17 14:58:40 +0000

    added Nine.java

--- public/javaPrograms/functions/Nine.java
+++ public/javaPrograms/functions/Nine.java
@@ -1,4 +1,19 @@
 package javaPrograms.functions;
-class Nine{
 
+import java.util.Scanner;
+
+class Nine{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int fact = factorial(n);
+        System.out.println(fact);
+    }
+static int factorial(int n){
+    int mul = 1,i;
+    for(i=n;i>0;i--){
+        mul = mul*i;
+    }
+    return mul;
+}
 }
