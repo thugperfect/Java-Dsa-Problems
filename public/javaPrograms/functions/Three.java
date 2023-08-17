b0c27f9612cd6f919cf39b401919a3b0461e8d7c
thugperfect <meenamani9444@gmail.com>
2023-08-17 13:24:11 +0000

    added Three.java

--- public/javaPrograms/functions/Three.java
+++ public/javaPrograms/functions/Three.java
@@ -1,4 +1,23 @@
 package javaPrograms.functions;
-class Three{
 
+import java.util.Scanner;
+
+class Three{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n =  s.nextInt();
+        boolean vote = vote(n);
+        if(vote){
+            System.out.println("HE CAN VOTE");
+        }else{
+            System.out.println("NO,HE CAN'T");
+        }
+    }
+    static boolean vote(int n){
+        if(n>=18){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
