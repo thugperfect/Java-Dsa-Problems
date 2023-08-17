574fbdd9331720e4c490913977793821aa4ef041
thugperfect <meenamani9444@gmail.com>
2023-08-17 13:34:37 +0000

    added Five .java

--- public/javaPrograms/functions/Five.java
+++ public/javaPrograms/functions/Five.java
@@ -1,4 +1,17 @@
 package javaPrograms.functions;
-class Five{
 
-}
+import java.util.Scanner;
+
+class Five{
+  
+        public static void main(String[] args) {
+            Scanner s = new Scanner(System.in);
+            int a = s.nextInt();
+            int b= s.nextInt();
+            int mul = mul(a,b);
+            System.out.println("MUL="+mul);
+        }
+    static int mul(int a ,int b){
+        return a*b;
+    }
+    }
