5008c92fddc6e84066c1e9841ddca815a71681d7
thugperfect <meenamani9444@gmail.com>
2023-08-17 13:10:17 +0000

    added One.java

--- public/javaPrograms/functions/One.java
+++ public/javaPrograms/functions/One.java
@@ -1,4 +1,50 @@
 package javaPrograms.functions;
-class One{
 
+import java.util.Scanner;
+
+class One{
+    public static void main(String[] args) {
+        Scanner s = new Scanner (System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+        int min = min(a,b,c);
+        int max = max(a,b,c);
+        System.out.println("MIN:"+min);
+        System.out.println("MAX:"+max);
+    }
+    static int max(int a,int b,int c){
+        if(a>b){
+            if(a>c){
+                return a;
+            }
+            else{
+                return c;
+            }
+        }else{
+            if(b>c){
+                return b;
+            }
+            else{
+                return c;
+            }
+        }
+    }
+     static int min(int a,int b,int c){
+        if(a<b){
+            if(a<c){
+                return a;
+            }
+            else{
+                return c;
+            }
+        }else{
+            if(b<c){
+                return b;
+            }
+            else{
+                return c;
+            }
+        }
+    }
 }
