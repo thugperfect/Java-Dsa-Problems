c98f41630f6077a3572310bff47545af7aba7ec9
thugperfect <meenamani9444@gmail.com>
2023-08-17 15:56:20 +0000

    added Twelve.java

--- public/javaPrograms/functions/Thirteen.java
+++ public/javaPrograms/functions/Thirteen.java
@@ -3,30 +3,18 @@ package javaPrograms.functions;
 import java.util.Scanner;
 
 class Thirteen{
-public static void main(String[] args) {
-    Scanner s = new Scanner(System.in);
-    int min = s.nextInt();
-    int max = s.nextInt();
-    for(int i = min;i<=max;i++){
- prime(i);
-    }
-   
-}
-static void prime(int a){
- int flag = 0;
-for(int i = 2;i<a/2;i++){
-   
-    if(a%i == 0){
-        flag = 0;
-        break;
-    }
-    else{
-        flag = 1;
-    }  
-}
- if(a == 2||a==3||a==5||flag ==1){
-        System.out.print(a+" ");
-    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int limit = s.nextInt();
+        int sum = sum(limit);
+        System.out.println("SUM="+sum);
 
-}
+    }
+    static int sum(int n){
+        int sum = 0;
+        for(int i = 0;i<=n;i++){
+            sum = sum+i;
+        }
+        return sum;
+    }
 }
