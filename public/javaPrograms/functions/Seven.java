db1547d9f99cc4a75a8ea7b89093a9e93e8b2445
thugperfect <meenamani9444@gmail.com>
2023-08-17 13:54:05 +0000

    added Seven.java

--- public/javaPrograms/functions/Seven.java
+++ public/javaPrograms/functions/Seven.java
@@ -1,4 +1,27 @@
 package javaPrograms.functions;
+
+import java.util.Scanner;
+
 class Seven{
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        fib(n);
+        System.out.println(" ");
+        fibb(n);
+    }
+static void fib(int n){
+    int a =0;
+    int b = 1,i;
+    System.out.print(b+" ");
+    for(i= 0;i<n;i++){
+        int c = a+b;
+        a=b;b=c;
+        System.out.print(c+" ");
+    }
+}
+
+
 
 }
