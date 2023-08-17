99b140b9f51463344f22d4b0a8c88521d81c7522
thugperfect <meenamani9444@gmail.com>
2023-08-17 15:13:22 +0000

    added Ten.java

--- public/javaPrograms/functions/Ten.java
+++ public/javaPrograms/functions/Ten.java
@@ -1,4 +1,27 @@
 package javaPrograms.functions;
-class Ten{
 
+import java.util.Scanner;
+
+class Ten{
+    public static void main(String[] args) {
+       Scanner s= new Scanner (System.in);
+       int n = s.nextInt();
+       boolean pal = isPalindrome(n);
+       System.out.println(pal);
+    }
+static boolean isPalindrome(int n){
+    int sep,clone =n,rev =0;
+    while(clone>0){
+        sep  = clone%10;
+        clone/=10;
+        rev = rev*10+sep;
+    }
+    if(n==rev){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+}
 }
