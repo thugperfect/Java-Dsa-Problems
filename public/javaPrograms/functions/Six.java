646f9d0da72c85f0f1f87555a5d438c20a73ea53
thugperfect <meenamani9444@gmail.com>
2023-08-17 13:39:30 +0000

    added Six.java

--- public/javaPrograms/functions/Six.java
+++ public/javaPrograms/functions/Six.java
@@ -1,4 +1,22 @@
 package javaPrograms.functions;
-class Six{
 
+import java.util.Scanner;
+
+class Six{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n= s.nextInt();
+        int area = area(n);
+        int circum = circum(n);
+        System.out.println("AREA="+area);
+        System.out.println("CIRCUMFERENCE="+circum);
+    }
+static int area(int r){
+    int area = 22/7*r*r;
+    return area;
+}
+static int circum(int r){
+    int circum = 2*22/7*r;
+    return circum;
+}
 }
