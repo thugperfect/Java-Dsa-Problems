7dc60f2cfef05da7a7750be86b1eb096d899412f
thugperfect <meenamani9444@gmail.com>
2023-08-17 13:20:21 +0000

    added Two.java

--- public/javaPrograms/functions/Two.java
+++ public/javaPrograms/functions/Two.java
@@ -1,4 +1,26 @@
 package javaPrograms.functions;
-class Two{
 
+import java.util.Scanner;
+
+class Two{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        boolean oddoreven = oddOrEven(n);
+        if(oddoreven){
+            System.out.println("ODD");
+        }
+        else{
+            System.out.println("EVEN");
+        }
+
+    }
+static boolean oddOrEven(int n){
+    if(n%2 ==0){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
 }
