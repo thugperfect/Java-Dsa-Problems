fb7e146f072761335a8f3d6a2be17ecc58bbda2b
thugperfect <meenamani9444@gmail.com>
2023-08-28 14:57:05 +0530

    page fetching logic

--- src/programs/ArraysMain.js
+++ src/programs/ArraysMain.js
@@ -1,5 +1,5 @@
 const ArraysMain = () =>{
-    return(<div className="w-4/5 container hidden flex flex-col items-center">
+    return(<div className="w-4/5 container  flex flex-col items-center">
     <div className="h-10 w-4/5 bg-slate-600 px-4 mb-1 rounded-lg">
         1.
     </div>
--- src/topics/All.js
+++ src/topics/All.js
@@ -4,47 +4,71 @@ import ConditionalsMain from '../programs/ConditionalsMain'
 import FunctionsMain from '../programs/FunctionsMain'
 import ArraysMain from '../programs/ArraysMain'
 import Arrays2Main from '../programs/Arrays2Main'
-import { useState } from 'react'
+import { useState ,useEffect} from 'react'
 const All = () =>{
 
     const [Firstjava ,changeFirstjava] = useState()
+    const [conditionals ,changeConditionals] = useState()
+    const [functions ,changeFunctions] = useState()
+    const [arrays ,changeArrays] = useState()
+    const [arrays2 ,changeArrays2] = useState()
+    const [click,setClick] = useState(0)
+    
+        const Changepage =(prop) =>{
+            console.log(prop);
+            if(click !== prop){
+                setClick(prop)
+            }else{
+                setClick(0)
+            }
+            
+            
+        }
 
-    const Changepage =() =>{
-        changeFirstjava(<FirstjavaMain/>)
-    }
+   
+    
+    // const ChangepageFirst =() =>{
+    //     changeFirstjava(<FirstjavaMain/>)
+    // }
    return(<div className="container lg:w-4/5 text-white  px-5 mx-auto  bold italic ">
     <div className="topbar bg-red-600 w-full h-16 flex justify-center items-center text-2xl">
         Java-DSA
     </div>
     <div className="container mx-auto w-full bg-zinc-800 lg:text-lg md:text-md flex flex-col items-center">
    <div className="w-4/5 bg-slate-700 px-5 h-16 cursor-pointer flex justify-between items-center mb-1 rounded-lg"
-   onClick={Changepage}
+   onClick={()=>Changepage(1)}
    >
     <div>First Java</div>
     <img className='w-auto h-full bg-yellow-500' src={downArrow} alt="nothing" />
    </div>
-   {Firstjava}
-   <div className="w-4/5 bg-slate-700 px-5 h-16 cursor-pointer flex  justify-between items-center mb-1 rounded-lg">
+   {(click===1)?<FirstjavaMain/>:""}
+   <div className="w-4/5 bg-slate-700 px-5 h-16 cursor-pointer flex  justify-between items-center mb-1 rounded-lg"
+   onClick={()=>Changepage(2)}>
    <div>Conditionals & Loops</div>
    <img className='w-auto h-full bg-yellow-500' src={downArrow} alt="nothing" />
    </div>
-   
+  
+   {(click===2)?<ConditionalsMain/>:""}
 
-   <div className="w-4/5 bg-slate-700 px-5 h-16 cursor-pointer flex justify-between items-center mb-1 rounded-lg">
+   <div className="w-4/5 bg-slate-700 px-5 h-16 cursor-pointer flex justify-between items-center mb-1 rounded-lg"
+   onClick={()=>Changepage(3)}>
    <div>Functions</div>
    <img className='w-auto h-full bg-yellow-500' src={downArrow} alt="nothing" />
    </div>
-   
-   <div className="w-4/5 bg-slate-700 px-5 h-16 cursor-pointer flex  justify-between items-center mb-1 rounded-lg">
+   {(click===3)?<FunctionsMain/>:""}
+   <div className="w-4/5 bg-slate-700 px-5 h-16 cursor-pointer flex  justify-between items-center mb-1 rounded-lg"
+   onClick={()=>Changepage(4)}>
    <div>Arrays</div>
    <img className='w-auto h-full bg-yellow-500' src={downArrow} alt="nothing" />
    </div>
+   {(click===4)?<ArraysMain/>:""}
    
-   <div className="w-4/5 bg-slate-700 px-5 h-16 cursor-pointer flex  justify-between items-center mb-1 rounded-lg">
+   <div className="w-4/5 bg-slate-700 px-5 h-16 cursor-pointer flex  justify-between items-center mb-1 rounded-lg" 
+   onClick={()=>Changepage(5)}>
    <div>Arrays 2</div>
    <img className='w-auto h-full bg-yellow-500' src={downArrow} alt="nothing" />
    </div>
-   
+   {(click===5)?<Arrays2Main/>:""}
    <div className="w-4/5 bg-slate-700 px-5 h-16 cursor-pointer flex  justify-between items-center mb-1 rounded-lg">
    <div>Searching</div>
    <img className='w-auto h-full bg-yellow-500' src={downArrow} alt="nothing" />
