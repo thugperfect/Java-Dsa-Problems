24557f82dbaad363f872c9a7cc6871b4b98253b6
thugperfect <meenamani9444@gmail.com>
2023-08-28 10:21:05 +0530

    new styles

---
+++ src/programs/Programsmain.js
@@ -0,0 +1,35 @@
+const Programsmain = () =>{
+   return(<div className="w-4/5 container flex flex-col items-center">
+    <div className="h-10 w-4/5 bg-slate-600 px-4 mb-1 rounded-lg">
+        1.
+    </div>
+    <div className="h-10 w-4/5 bg-slate-600 px-4 mb-1 rounded-lg">
+        2.
+    </div>
+    <div className="h-10 w-4/5 bg-slate-600 px-4 mb-1 rounded-lg">
+        3.
+    </div>
+    <div className="h-10 w-4/5 bg-slate-600 px-4 mb-1 rounded-lg">
+        4.
+    </div>
+    <div className="h-10 w-4/5 bg-slate-600 px-4 mb-1 rounded-lg">
+        5.
+    </div>
+    <div className="h-10 w-4/5 bg-slate-600 px-4 mb-1 rounded-lg">
+        6.
+    </div>
+    <div className="h-10 w-4/5 bg-slate-600 px-4 mb-1 rounded-lg">
+        7.
+    </div>
+    <div className="h-10 w-4/5 bg-slate-600 px-4 mb-1 rounded-lg">
+        8.
+    </div>
+    <div className="h-10 w-4/5 bg-slate-600 px-4 mb-1 rounded-lg">
+        9.
+    </div>
+  
+   
+   </div>) 
+}
+
+export default Programsmain
--- src/topics/All.js
+++ src/topics/All.js
@@ -1,14 +1,16 @@
 import downArrow from '../images/expand_more_FILL0_wght400_GRAD0_opsz24.svg'
+import Programsmain from '../programs/Programsmain'
 const All = () =>{
    return(<div className="container lg:w-4/5 text-white  px-5 mx-auto  bold italic ">
     <div className="topbar bg-red-600 w-full h-16 flex justify-center items-center text-2xl">
         Java-DSA
     </div>
-    <div className=" w-full bg-yellow-200 lg:text-lg md:text-md flex flex-col items-center">
+    <div className="container mx-auto w-full bg-zinc-800 lg:text-lg md:text-md flex flex-col items-center">
    <div className="w-4/5 bg-slate-700 px-5 h-16 cursor-pointer flex justify-between items-center mb-1 rounded-lg">
     <div>First Java</div>
     <img className='w-auto h-full bg-yellow-500' src={downArrow} alt="nothing" />
    </div>
+   <Programsmain/>
    <div className="w-4/5 bg-slate-700 px-5 h-16 cursor-pointer flex  justify-between items-center mb-1 rounded-lg">
    <div>Conditionals & Loops</div>
    <img className='w-auto h-full bg-yellow-500' src={downArrow} alt="nothing" />
