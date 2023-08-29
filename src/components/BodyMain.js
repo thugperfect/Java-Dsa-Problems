0ea9a3a8cfef13e28203f3568d9b6fabd0a88844
thugperfect <meenamani9444@gmail.com>
2023-08-29 13:44:24 +0530

    style changed

--- src/components/BodyMain.js
+++ src/components/BodyMain.js
@@ -3,7 +3,7 @@ import React, { useState } from "react"
 const BodyMain = ({topic,onProgramChange}) =>{
 
  const passedTopic = React.cloneElement(topic,{onProgramChange})
-    return(<div className="container lg:w-4/5 text-white  px-5 mx-auto  bold italic ">
+    return(<div className="container lg:w-4/5 text-white  px-5 mx-auto  bold italic h-full">
     <div className="topbar bg-red-600 w-full h-16 flex justify-center items-center text-2xl">
         Java-DSA
     </div>
--- src/topics/PageChanger.js
+++ src/topics/PageChanger.js
@@ -1,7 +1,20 @@
+import { moduleFetch } from "../moduleFetch"
 const PageChanger = ({mod}) =>{
+    const numberMod = Number(mod)
+
+     const file = moduleFetch.find((prop)=>
+         prop.id ===numberMod
+
+    )
+    console.log(file);
    
     return(
-        <div className="text-white">Hola</div>
+        <div className="container text-white flex flex-col items-center gap-8 min-h-[700px]">
+            <div className="mt-5">{file.qn_no}.{file.qn}</div>
+            <div className="container bg-black w-4/5 h-auto rounded-lg px-4 p-4">
+         
+       </div>
+            </div>
     )
 }
 export default PageChanger
