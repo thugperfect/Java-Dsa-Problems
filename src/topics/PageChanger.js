b3a7a2dca832b0346029070f4e9767b9bb2916b7
thugperfect <meenamani9444@gmail.com>
2023-08-29 14:02:28 +0530

    all first java fetch file complete

--- src/topics/PageChanger.js
+++ src/topics/PageChanger.js
@@ -1,19 +1,34 @@
 import { moduleFetch } from "../moduleFetch"
-const PageChanger = ({mod}) =>{
-    const numberMod = Number(mod)
+import { useState,useEffect } from "react"
 
-     const file = moduleFetch.find((prop)=>
+const PageChanger = ({mod}) =>{
+
+const numberMod = Number(mod)
+const url = window.location.href
+const fileFetch = moduleFetch.find((prop)=>
          prop.id ===numberMod
 
     )
-    console.log(file);
-   
+    const fileName = fileFetch.file_name
+    const [file,setFile]= useState("Loading...")
+    useEffect(()=>{
+        const fetchFile = async ()=>{
+            try{
+                const res = await fetch(`${url}javaPrograms/${fileName}`)
+                const content = await res.text()
+                setFile(content)
+            }catch{
+
+            }
+        }
+        fetchFile()
+    },[])
     return(
         <div className="container text-white flex flex-col items-center gap-8 min-h-[700px]">
-            <div className="mt-5">{file.qn_no}.{file.qn}</div>
-            <div className="container bg-black w-4/5 h-auto rounded-lg px-4 p-4">
-         
-       </div>
+            <div className="mt-5">{fileFetch.qn_no}.{fileFetch.qn}</div>
+            <pre className="container bg-black w-4/5 h-auto rounded-lg px-4 p-4">
+         {file}
+       </pre>
             </div>
     )
 }
