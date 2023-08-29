d5463e1116bb82d9f4dfbe56f5dd8a06c2853a45
thugperfect <meenamani9444@gmail.com>
2023-08-29 10:56:33 +0530

    cleaning code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import Body from './components/Body';
 import './styles/App.css';
-import Navbar from './components/Navbar';
-import Topbar from './components/Topbar';
-import FirstJava from './topics/FirstJava';
 import BodyMain from './components/BodyMain';
 import All from './topics/All';
-
-
-import New from './New';
 
 import { useState } from "react"
 function App() {
 
   const [topic,changeTopic] = useState(<All/>)
 
-  const handleTopicChange = (newTopic) =>{
-    changeTopic(newTopic)
-  }
 
   const handleProgramChange = (newProgram) =>{
     changeTopic(newProgram)
---
+++ src/topics/PageChanger.js
@@ -0,0 +1,7 @@
+const PageChanger = ({mod}) =>{
+   
+    return(
+        <div className="text-white">HOLa</div>
+    )
+}
+export default PageChanger
