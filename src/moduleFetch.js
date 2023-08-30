b413f9b19f21c135452a2870edb99477e5ea2b7a
thugperfect <meenamani9444@gmail.com>
2023-08-30 08:20:40 +0530

    added conditionals json

--- src/moduleFetch.js
+++ src/moduleFetch.js
@@ -53,5 +53,132 @@ const moduleFetch = [
         qn:"To find Armstrong Number between two given number.",
         file_name:"firstJava/Nine.java"  
     },
+    {
+        qn_no:1,
+        id:2.1,
+        qn:"Fibonacci Series In Java Programs",
+        file_name:"conditionalsAndLoops/One.java"  
+    },
+    {
+        qn_no:2,
+        id:2.2,
+        qn:"Subtract the Product and Sum of Digits of an Integer",
+        file_name:"conditionalsAndLoops/Two.java"  
+    },
+    {
+        qn_no:3,
+        id:2.3,
+        qn:"Input a number and print all the factors of that number (use loops).",
+        file_name:"conditionalsAndLoops/Three.java"  
+    },
+    {
+        qn_no:4,
+        id:2.4,
+        qn:"Take integer inputs till the user enters 0 and print the sum of all numbers (HINT: while loop)",
+        file_name:"conditionalsAndLoops/Four.java"  
+    },
+    {
+        qn_no:5,
+        id:2.5,
+        qn:"Take integer inputs till the user enters 0 and print the largest number from all.",
+        file_name:"conditionalsAndLoops/Five.java"  
+    },
+    {
+        qn_no:6,
+        id:2.6,
+        qn:"Factorial Program In Java",
+        file_name:"conditionalsAndLoops/Six.java"  
+    },
+    {
+        qn_no:7,
+        id:2.7,
+        qn:"Calculate Electricity Bill",
+        file_name:"conditionalsAndLoops/Seven.java"  
+    },
+    {
+        qn_no:8,
+        id:2.8,
+        qn:"Calculate Average Of N Numbers",
+        file_name:"conditionalsAndLoops/Eight.java"  
+    },
+    {
+        qn_no:9,
+        id:2.9,
+        qn:"Calculate Discount Of Product",
+        file_name:"conditionalsAndLoops/Nine.java"  
+    },
+    {
+        qn_no:10,
+        id:2.10,
+        qn:"Calculate Distance Between Two Points",
+        file_name:"conditionalsAndLoops/Ten.java"  
+    },
+    {
+        qn_no:11,
+        id:2.11,
+        qn:"Calculate Commission Percentage",
+        file_name:"conditionalsAndLoops/Eleven.java"  
+    },
+    {
+        qn_no:12,
+        id:2.12,
+        qn:"Calculate CGPA Java Program",
+        file_name:"conditionalsAndLoops/Twelve.java"  
+    },
+    {
+        qn_no:13,
+        id:2.13,
+        qn:"Sum Of N Numbers",
+        file_name:"conditionalsAndLoops/Thirteen.java"  
+    },
+    {
+        qn_no:14,
+        id:2.14,
+        qn:"Armstrong Number In Java",
+        file_name:"conditionalsAndLoops/Fourteen.java"  
+    },
+    {
+        qn_no:15,
+        id:2.15,
+        qn:"Find Ncr & Npr",
+        file_name:"conditionalsAndLoops/Fifteen.java"  
+    },
+    {
+        qn_no:16,
+        id:2.16,
+        qn:"Reverse A String In Java",
+        file_name:"conditionalsAndLoops/Sixteen.java"  
+    },
+    {
+        qn_no:17,
+        id:2.17,
+        qn:"Find if a number is palindrome or not",
+        file_name:"conditionalsAndLoops/Seventeen.java"  
+    },
+    {
+        qn_no:18,
+        id:2.18,
+        qn:"HCF & LCM Of Two Numbers Program",
+        file_name:"conditionalsAndLoops/Eighteen.java"  
+    },
+    {
+        qn_no:19,
+        id:2.19,
+        qn:"Check Leap Year Or Not",
+        file_name:"conditionalsAndLoops/Nineteen.java"  
+    },
+    {
+        qn_no:20,
+        id:2.20,
+        qn:"Write a program to print the sum of negative numbers, sum of positive even numbers and the sum of positive odd numbers from a list of numbers (N) entered by the user. The list terminates when the user enters a zero.",
+        file_name:"conditionalsAndLoops/Twenty.java"  
+    },
+    {
+        qn_no:21,
+        id:2.21,
+        qn:"Kunal is allowed to go out with his friends only on the even days of a given month. Write a program to count the number of days he can go out in the month of August.",
+        file_name:"conditionalsAndLoops/Twentyone.java"  
+    },
+
 ]
 module.exports = {moduleFetch}
